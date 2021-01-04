9fb8756a5aa82e8215b52460cf6facac9510911c
sjhan2781 <sjhan2781@gmail.com>
2021-01-05 05:33:58 +0900

    앱 아이콘 추가

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
+import os
 import sys
 
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 
 from controller import StartController
@@ -9,15 +11,20 @@ from start_view import StartView
 class App(QApplication):
     def __init__(self, sys_argv):
         super(App, self).__init__(sys_argv)
-        # self.model = Model()
+
+        self.setWindowIcon(QIcon(self.resource_path('./image/logo.ico')))
+
         self.main_controller = StartController()
         self.main_view = StartView(self.main_controller)
-        # self.main_view = StartView()
-        # self.loading_view = LoadingWidgetTest("aaaa")
+
         self.main_view.show()
+
+    def resource_path(self, relative_path):
+        if hasattr(sys, '_MEIPASS'):
+            return os.path.join(sys._MEIPASS, relative_path)
+        return os.path.join(os.path.abspath("."), relative_path)
 
 
 if __name__ == '__main__':
     app = App(sys.argv)
-    app.exec()
-    # sys.exit(app.exec_())
+    sys.exit(app.exec_())
